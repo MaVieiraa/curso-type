69c424432c611307221cc7fdb4d7666cb0cb27a5
unknown <marialuisadiasvieira0@gmail.com>
2023-11-03 12:41:25 -0300

    resolvendo

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export const AppRoutes = () => {
 
  return(
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página Inicial</p>}/>
+      <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { AppRoutes } from './routes';
+import { AppRoutes } from '../routes';
 
 export const App = () => {
   return (
