5a411256c4904572115ee4f3a63a63dd64498b03
unknown <marialuisadiasvieira0@gmail.com>
2023-11-03 17:54:20 -0300

    alterações

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,28 @@
+import { createTheme } from '@mui/material';
+import { cyan, pink } from '@mui/material/colors';
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: pink [700],
+            dark: pink [200], 
+            light: '[100]',
+            contrastText: '#ffffff',
+
+        },
+
+        secondary: {
+            main:  '[500]',
+            dark: pink [100], 
+            light: '[50]',
+            contrastText: '#ffffff',
+           
+        },
+
+        background: {
+            default: '#202124',
+            paper:'#303134', 
+
+        }
+    }
+});
